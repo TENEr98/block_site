1fed1454d6874e7f326cf5625c5df507e168f849
TENEr98 <levon.tarayan.98@mail.ru>
2023-12-05 13:13:06 +0500

    fix: remove unused hello world get api

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
 
 import { DbService } from './db/db.service';
-
-class HelloWorldDto {
-  @ApiProperty()
-  message: string;
-}
 
 @Controller()
 export class AppController {
@@ -15,14 +9,4 @@ export class AppController {
     private readonly appService: AppService,
     private dbService: DbService,
   ) {}
-
-  @Get()
-  @ApiOkResponse({
-    type: HelloWorldDto,
-  })
-  async getHello(): Promise<HelloWorldDto> {
-    const users = await this.dbService.user.findMany({});
-    console.log(users);
-    return { message: this.appService.getHello() };
-  }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DbService } from './db/db.service';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
 import { AccountModule } from './account/account.module';
 import { BlockListModule } from './block-list/block-list.module';
 
