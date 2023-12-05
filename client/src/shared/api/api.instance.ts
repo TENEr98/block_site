de9852ec6e57b9a8211bfca40d335faba04590f4
TENEr98 <levon.tarayan.98@mail.ru>
2023-12-05 13:12:12 +0500

    init: client react-query orval next prettier configuration complite

---
+++ client/src/shared/api/api.instance.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+
+export const apiInstance = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const createInstance = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig
+): Promise<T> =>
+  apiInstance({
+    ...config,
+    ...options,
+  }).then((response) => response?.data);
+
+export type BodyType<Data> = Data;
+
+export type ErrorType<Error> = AxiosError<Error>;
